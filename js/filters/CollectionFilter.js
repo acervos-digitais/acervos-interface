ee45061671d5efab6a75b3190ee4354a1c7f4be1
Thiago Hersan <thiago.hersan+github@gmail.com>
2025-08-20 20:28:40 -0300

    adjustments to full version

--- js/filters/CollectionFilter.js
+++ js/filters/CollectionFilter.js
@@ -16,8 +16,11 @@ class CollectionFilter extends Filter {
     const selectedVals = this.inputs.filter(el => el.checked).map(el => el.value);
 
     if (selectedVals.length < 1) {
-      return new Set();
-      return inIdsSet;
+      if (window.location.href.includes("full")) {
+        return inIdsSet;
+      } else {
+        return new Set();
+      }
     }
 
     const selectedIdsSet = selectedVals.reduce((acc, val) => acc.union(new Set(this.data[val])), new Set());
